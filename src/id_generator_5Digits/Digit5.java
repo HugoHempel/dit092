e9b5f96bd1e9ecdd3a02d6c966dd30a9a9776e9f
altug <gusaltetal@student.gu.se>
2019-11-22 13:44:26 +0200

    Testing git with IntelliJ

--- src/id_generator_5Digits/Digit5.java
+++ src/id_generator_5Digits/Digit5.java
@@ -8,6 +8,7 @@ public class Digit5 {
     private int ID;
     private String maskID;
 
+    // TODO: Remove this comment
     public Digit5 (String firstName, String lastName, String maskID, int birthday, int ID) {
         this.firstName = firstName;
         this.lastName = lastName;
