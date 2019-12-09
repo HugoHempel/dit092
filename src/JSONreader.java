fd1c060d601344b2cd1ec428de50ae69d7674e0d
hjathu <hjathu@student.chalmers.se>
2019-12-09 23:51:51 +0100

    Fully completed JSONreader class which can now read, create objects and store them so the program can utilize the values for calculations etc.

--- src/JSONreader.java
+++ src/JSONreader.java
@@ -1,6 +1,8 @@
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -9,76 +11,61 @@ import org.json.simple.parser.ParseException;
 
 public class JSONreader {
 
-    //TODO complete the JSONreader file where we will read the input file and create all objects to preload the program with
-
-    //Loads in input file and creates a planner object
-    public void loadPlanner() {
+    public Planner loadPlanner() throws IOException, ParseException {
 
         JSONParser parser = new JSONParser();
+        FileReader reader = new FileReader("input.json");
+        Object obj = parser.parse(reader);
 
-        try (FileReader reader = new FileReader("input.json")) {
+        JSONObject jsonObject = (JSONObject) obj;
+        String projectName = (String) jsonObject.get("projectName");
+        LocalDate startDate = LocalDate.parse((String) jsonObject.get("startDate"));
+        LocalDate endDate = LocalDate.parse((String) jsonObject.get("endDate"));
+        Planner planner = new Planner(projectName, startDate, endDate);
 
-            Object obj = parser.parse(reader);                   //Parse the content in input.json
+        List<Employee> employeeListTemp = parseEmployeeObject(jsonObject);
+        planner.storeEmployees(employeeListTemp);
 
+        List<Milestones> milestoneListTemp = parseMilestoneObject(jsonObject);
+        planner.storeMilestones(milestoneListTemp);
 
-            JSONObject jsonObject = (JSONObject) obj;            //Convert above information into a json object
-
-            //Create and print object for planner
-            String projectName = (String) jsonObject.get("projectName");
-            String projectStartDate = (String) jsonObject.get("startDate");
-            LocalDate startDate = LocalDate.parse(projectStartDate);
-            String projectEndDate = (String) jsonObject.get("endDate");
-            LocalDate endDate = LocalDate.parse(projectEndDate);
-            Planner planner = new Planner(projectName, startDate, endDate);
-            System.out.println(planner);
-
-            parseEmployeeObject(jsonObject);
-            parseMilestoneObject(jsonObject);
-
-        } catch (IOException | ParseException e) {
-            e.printStackTrace();
-        }
+        return planner;
     }
 
-    //Creates employee object(s) and puts into array.
-    private void parseEmployeeObject(JSONObject jsonObject) {
+    private List<Employee> parseEmployeeObject(JSONObject jsonObject) {
 
-        JSONArray objArray = (JSONArray) jsonObject.get("employeeList"); //Create the json array for employeeList in input.json
+        List<Employee> employeeListTemp = new ArrayList<>();
+        JSONArray objArray = (JSONArray) jsonObject.get("employeeList");
 
-        //Create and print object for employee
         for (int i = 0; i < objArray.size(); i++) {
             JSONObject arrayIn = (JSONObject) objArray.get(i);
             String foreName = (String) arrayIn.get("employeeForename");
             String surName = (String) arrayIn.get("employeeSurname");
-            String birth = (String) arrayIn.get("employeeBirth");
-            int birthDay = Integer.parseInt(birth);
-            String salary = (String) arrayIn.get("salary");
-            double employeeSalary = Double.parseDouble(salary);
+            int birthDay = Integer.parseInt((String) arrayIn.get("employeeBirth"));
+            double employeeSalary = Double.parseDouble((String) arrayIn.get("salary"));
 
-            Employee employees = new Employee(foreName, surName, birthDay, employeeSalary);
-            System.out.println(employees);
+            Employee employee = new Employee(foreName, surName, birthDay, employeeSalary);
+            employeeListTemp.add(employee);
         }
+        return employeeListTemp;
     }
-    //Creares milestone object(sw) and puts into array.
-    private void parseMilestoneObject(JSONObject jsonObject) {
 
-        JSONArray objArray = (JSONArray) jsonObject.get("milestoneList");  //Create the json array for milestoneList in input.json
+    private List<Milestones> parseMilestoneObject(JSONObject jsonObject) {
 
-        //Create and print object for employee
-        //Also works for the maps key and value relationship
+        List<Milestones> milestoneListTemp = new ArrayList<>();
+        JSONArray objArray = (JSONArray) jsonObject.get("milestoneList");
+
         for (int i = 0; i < objArray.size(); i++) {
             JSONObject arrayIn = (JSONObject) objArray.get(i);
             String taskName = (String) arrayIn.get("taskName");
-            String taskStartDate = (String) arrayIn.get("startDate");
-            LocalDate startDate = LocalDate.parse(taskStartDate);
-            String plannedEnd = (String) arrayIn.get("plannedEndDate");
-            LocalDate plannedEndDate = LocalDate.parse(plannedEnd);
-            String actualEnd = (String) arrayIn.get("actualEndDate");
-            LocalDate actualEndDate = LocalDate.parse(actualEnd);
+            LocalDate startDate = LocalDate.parse((String) arrayIn.get("startDate"));
+            LocalDate plannedEndDate = LocalDate.parse((String) arrayIn.get("plannedEndDate"));
+            LocalDate actualEndDate = LocalDate.parse((String) arrayIn.get("actualEndDate"));
             JSONObject taskMemberMap = (JSONObject) arrayIn.get("hoursPerEmployee");
 
-            Milestones milestone = new Milestones(taskName,startDate, plannedEndDate, actualEndDate, taskMemberMap);
-            System.out.println(milestone);
+            Milestones milestone = new Milestones(taskName, startDate, plannedEndDate, actualEndDate, taskMemberMap);
+            milestoneListTemp.add(milestone);
         }
+        return milestoneListTemp;
     }
 }
