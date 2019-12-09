094c9e8d6ae36644bc3cca1916e2a03e5f137709
hjathu <hjathu@student.chalmers.se>
2019-12-09 10:35:12 +0100

    Fixed milestones class with all the attributes we need, only methods left

--- src/Milestones.java
+++ src/Milestones.java
@@ -1,18 +1,21 @@
 import java.time.LocalDate;
+import java.util.Map;
 
 public class Milestones {
-    //TODO figure out how to divide hours for each employee so we can see who spent time on what
+//TODO start working on the calculation methods of hours spent by users for each task etc which will be used by the planner class
 
     String name;
-    LocalDate start;
-    LocalDate plannedEnd;
-    LocalDate actualEnd;
-    float hours;
+    LocalDate startDate;
+    LocalDate plannedEndDate;
+    LocalDate actualEndDate;
 
-    public Milestones(String name, LocalDate start, LocalDate plannedEnd, LocalDate actualEnd, float hours){
+    Map<Integer, Double> employeeHours; //ID as the key, amount of hour as the value. ex. ID 1, hours 8
+
+    public Milestones(String name, LocalDate startDate, LocalDate plannedEndDate, LocalDate actualEndDate, Map <Integer, Double> employeeHours){
         this.name = name;
-        this.start = start;
-        this.plannedEnd = plannedEnd;
-        this.actualEnd = actualEnd;
+        this.startDate = startDate;
+        this.plannedEndDate = plannedEndDate;
+        this.actualEndDate = actualEndDate;
+        this.employeeHours = employeeHours;
     }
 }
