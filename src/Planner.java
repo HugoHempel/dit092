ee977bb120b6f702a65a244422501a6cc296c8c7
hjathu <hjathu@student.chalmers.se>
2019-12-09 23:52:40 +0100

    Fixed methods so we now store milestones and employees withing planner class and also print them

--- src/Planner.java
+++ src/Planner.java
@@ -1,17 +1,19 @@
 import java.time.temporal.ChronoUnit;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Planner {
-    //TODO complete the calculation methods
+
+    //TODO: complete the calculation methods
 
      double budget;                // the budget should be calculated in accordance to our hourly rate and working days [Linus]
      String projectName;
      LocalDate startDate;
      LocalDate endDate;
 
-     ArrayList<Employee> employees = new ArrayList<>();
-     ArrayList<Milestones> milestones = new ArrayList<>();
+     List<Employee> employees = new ArrayList<>();
+     List<Milestones> milestones = new ArrayList<>();
 
      public Planner(String projectName, LocalDate startDate, LocalDate endDate){
           this.projectName = projectName;
@@ -19,36 +21,25 @@ public class Planner {
           this.endDate = endDate;
      }
 
-     /*
-     LocalDate localDate = LocalDate.now();  // creates a date based on todays date [Hjalmar]
-     float totalDays = ChronoUnit.DAYS.between(start, end); // compares 2 dates and returns the diff in numbers [Hjalmar]
-     float nowDays = ChronoUnit.DAYS.between(start, localDate);
-     float percentCompletePlanned = nowDays / totalDays;
-     double percentCompleteActual = 0.2; // here we have to sum the time for all tasks, sum all completed tasks and then divide them(?) [Hjalmar]
-
-     public double calcPV(){
-          return percentCompletePlanned * budget;
+     public void storeEmployees(List<Employee> employeeList){
+          this.employees = employeeList;
      }
 
-     public double calcEV(){
-          return percentCompleteActual * budget;
+     public void printEmployees(){
+          System.out.println(employees);
      }
 
-     public double calcSV(){
-          return calcEV() - calcPV();
+     public void storeMilestones(List<Milestones> MilestoneList){
+          this.milestones = MilestoneList;
      }
 
-     public double calcCV(){
-          double actualCost = 500;  // here we have to sum total cost we spent so far (all workers hours times salary)
-          return calcEV() - actualCost;
-     }*/
-
-     public void storeEmployee(Employee employee){
-          employees.add(employee);
+     public void printMilestones(){
+          System.out.println(milestones);
      }
+
      @Override
      public String toString() {
-          return "Name: " + projectName + "\n" +
+          return "\nName: " + projectName + "\n" +
                   "StartDate: " + startDate + "\n" +
                   "EndDate: " + endDate + "\n";
      }
