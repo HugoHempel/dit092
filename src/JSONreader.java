278e48dab458f0fe6ab754a158c9b5131e5b871f
hjathu <hjathu@student.chalmers.se>
2019-12-09 10:52:09 +0100

    Created a JSONreader class which we will use to read input file with

---
+++ src/JSONreader.java
@@ -0,0 +1,5 @@
+import org.json.simple.*;
+
+public class JSONreader {
+    //TODO complete the JSONreader file where we will read the input file and create all objects to preload the program with
+}
