3b79aa6f80f18f968dfc8d4107e5be60f8b0aadf
hugohe <Hugohhempel@gmail.com>
2019-12-10 09:26:27 +0100

    Generated getter/setter for Milestones class

--- src/Milestones.java
+++ src/Milestones.java
@@ -20,6 +20,46 @@ public class Milestones {
         this.employeeHours = employeeHours;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getPlannedEndDate() {
+        return plannedEndDate;
+    }
+
+    public void setPlannedEndDate(LocalDate plannedEndDate) {
+        this.plannedEndDate = plannedEndDate;
+    }
+
+    public LocalDate getActualEndDate() {
+        return actualEndDate;
+    }
+
+    public void setActualEndDate(LocalDate actualEndDate) {
+        this.actualEndDate = actualEndDate;
+    }
+
+    public Map<Integer, Double> getEmployeeHours() {
+        return employeeHours;
+    }
+
+    public void setEmployeeHours(Map<Integer, Double> employeeHours) {
+        this.employeeHours = employeeHours;
+    }
+
     @Override
     public String toString() {
         return "Name: " + name + "\n" +
